3fa0fc49be125c21d6b3768347fc0f6fb6998d75
fckmnd <168029387+fckmnd@users.noreply.github.com>
2024-04-30 13:31:23 +0300

    Add files via upload

--- i love cpipi.cpp
+++ i love cpipi.cpp
@@ -8,3 +8,24 @@ int main()
     cout<<"I love C++"<<endl;    
     return 0;    
 }  
+
+// Завдання 2 //
+
+int main()
+{
+	return 1;
+}
+
+
+//ЗАВДАННЯ 3,Баги які дає програма - tempCodeRunnerFile.cpp:6:31: error: missing terminating " character tempCodeRunnerFile.cpp: In function 'int main()': tempCodeRunnerFile.cpp:6:11: error: 'Is' was not declared in this scope Це означає в основному помилки синтаксису//
+
+//Завдання 4, виправила синтаксичні помилки і такий код отримала//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Is there a bug here ? \n";
+		return 0;
+}
